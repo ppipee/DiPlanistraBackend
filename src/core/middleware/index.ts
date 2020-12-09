774792933910afb990df4aa8e852dcb28707a169
ppipee <ppipee.kj@hotmail.com>
2020-12-09 23:20:29 +0700

    fix: update middleware

--- src/core/middleware/index.ts
+++ src/core/middleware/index.ts
@@ -1,17 +1,17 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import { Express, NextFunction, Request, Response } from 'express'
+import { Express } from 'express'
 
 import routes from 'routes'
 
-import allowHeader from './allowHeader'
+// import allowHeader from './allowHeader'
 
 export default function initMiddleware(app: Express) {
-	app.use(allowHeader)
+	// app.use(allowHeader)
 
 	app.use('/', routes)
 
-	app.use(cors)
+	app.use(cors())
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: false }))
 }
