0332a1cb6d312f8f3187b0d3f5137ddf2ac2eab3
ppipee <ppipee.kj@hotmail.com>
2020-12-09 07:23:59 +0700

    feat: set route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,8 @@ import { PLACES_PATH, USERS_PATH } from './constants'
 
 const router = express.Router()
 
+router.get('/', (req, res) => res.send('Di Planistra Sever'))
+
 router.use(PLACES_PATH, placeRoutes)
 router.use(USERS_PATH, userRoutes)
 
